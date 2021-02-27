b5d30e5b074938948dffd92ed77c53c2663ce0ba
mesutkoc <mestkoc34@gmail.com>
2021-02-28 00:56:12 +0300

    added class

--- src/components/LeftContentPanel.js
+++ src/components/LeftContentPanel.js
@@ -8,7 +8,7 @@ class LeftContentPanel extends Component {
   checkSelection(e) {
     const selection = e.target.parentNode.parentNode;
     if (
-      selection.childNodes[1].childNodes[0].innerText ===
+      selection.childNodes[0].childNodes[0].innerText ===
       this.props.userSelection
     ) {
       console.log(this.props.userSelection);
@@ -26,36 +26,36 @@ class LeftContentPanel extends Component {
 
           <ul className="list-group">
             <li className="list-group-item border-0">
-              <Link
-                to="/sharespage"
-                className="link"
-                onClick={(e) => this.checkSelection(e)}
-              >
-                <div className="row">
-                  <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="30"
-                      height="30"
-                      fill="currentColor"
-                      className="bi bi-house"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-                      />
-                      <path
-                        fillRule="evenodd"
-                        d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
-                      />
-                    </svg>
-                  </div>
+              <div className="row">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    fill="currentColor"
+                    className="bi bi-house"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
+                    />
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
+                    />
+                  </svg>
+                </div>
+                <Link
+                  to="/sharespage"
+                  className="link"
+                  onClick={(e) => this.checkSelection(e)}
+                >
                   <div>
                     <h6>Mainpage</h6>
                   </div>
-                </div>
-              </Link>
+                </Link>
+              </div>
             </li>
             <li className="list-group-item border-0">
               <Link to="/deneme" className="link">
